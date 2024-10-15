e84e6c332a28ce92999795b11aad0044b0bd570b
Yusuf Songur <95027542+moyeras@users.noreply.github.com>
2024-10-15 02:20:33 +0200

    Add files via upload

---
+++ DimCalendar.sql
@@ -0,0 +1,24 @@
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] as Date, 
+  --,[DayNumberOfWeek]
+  [EnglishDayNameOfWeek] as Day, 
+  --,[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  -- ,[DayNumberOfMonth]
+  --,[DayNumberOfYear]
+  [WeekNumberOfYear] as WeekNr, 
+  [EnglishMonthName] as Month, 
+  left([EnglishMonthName], 3) as MonthSort, 
+  --,[SpanishMonthName]
+  --,[FrenchMonthName]
+  [MonthNumberOfYear] as MonthNo, 
+  [CalendarQuarter] as Quarter, 
+  [CalendarYear] as Year --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimDate] 
+where 
+  CalendarYear >= 2019
